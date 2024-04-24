de890519d6496a1a7de4f02f91952ca71aa55c30
NATHAN BIZINOTO DE OLIVEIRA <1091392323023@fatec-srv.local>
2024-04-24 20:07:15 -0300

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const server = express();
+
+//localhost:3000/curso
+server.get('/curso', (req, res) => {
+    return res.json({curso: 'Node JS'});
+});
+
+server.listen(3000);
